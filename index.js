713f105092c4ba795f4c81f32ce933f81679c9c6
omar <omarsameh20017@gmail.com>
2025-01-01 16:13:43 +0200

    add nav and footer to home

--- index.js
+++ index.js
@@ -14,3 +14,36 @@
 // }
 
 // slideCategories();
+const navbarHtml = document.querySelector('.navbar');
+
+// Use fetch to load the HTML file
+fetch('./navbar/nav.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.text();
+    })
+    .then(html => {
+        navbarHtml.innerHTML = html;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
+const footerHtml = document.querySelector('.footer');
+
+// Use fetch to load the HTML file
+fetch('./footer/footer.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.text();
+    })
+    .then(html => {
+        footerHtml.innerHTML = html;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
