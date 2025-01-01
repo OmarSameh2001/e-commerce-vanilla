714274e891891b402e7b6a124e5c0b6c8a1d7f98
omar <omarsameh20017@gmail.com>
2025-01-01 20:06:46 +0200

    put navbar and footer everywhere

--- contact/contact.js
+++ contact/contact.js
@@ -1,13 +1,48 @@
+const form = document.getElementById('contact_form');
 
-  const form = document.getElementById('contact_form');
+form.addEventListener('submit', function(event) {
+
+  event.preventDefault();
   
-  form.addEventListener('submit', function(event) {
 
-    event.preventDefault();
-    
- 
-    alert("Your message has been sent!");
-    
+  alert("Your message has been sent!");
+  
 
-    form.reset();
-  });
+  form.reset();
+});
+
+// Navbar and footer-----------
+const navbarHtml = document.querySelector('.navbar');
+
+// Use fetch to load the HTML file
+fetch('../navbar/nav.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.text();
+    })
+    .then(html => {
+        navbarHtml.innerHTML = html;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
+const footerHtml = document.querySelector('.footer');
+
+// Use fetch to load the HTML file
+fetch('../footer/footer.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.text();
+    })
+    .then(html => {
+        footerHtml.innerHTML = html;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+// Navbar and footer---------------
