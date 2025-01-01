826776b08526469e76cea8458a54a46e97a537e2
omar <omarsameh20017@gmail.com>
2025-01-01 14:08:54 +0200

    create home

---
+++ index.js
@@ -0,0 +1,16 @@
+// const categories = document.querySelector('.categories');
+// let currentScroll = 0;
+// let scrollDirection = 1;
+
+// function slideCategories() {
+//     currentScroll += scrollDirection * 1;
+//     categories.scrollLeft = currentScroll;
+
+//     if (currentScroll >= categories.scrollWidth - categories.clientWidth || currentScroll <= 0) {
+//         scrollDirection *= -1;
+//     }
+
+//     requestAnimationFrame(slideCategories);
+// }
+
+// slideCategories();
