649a9b410ea6b745f12d2a246fd25af96b25167a
omar <omarsameh20017@gmail.com>
2025-01-06 00:31:01 +0200

    fix logout button in navbar

--- cart/cart.js
+++ cart/cart.js
@@ -50,6 +50,29 @@ fetch('../navbar/nav.html')
     .then(html => {
         navbarHtml.innerHTML = html;
     })
+    .then(() => {
+        const currentUser =
+          localStorage.getItem("currentUser").slice(1, -1)  ||
+          sessionStorage.getItem("currentUser").slice(1, -1) ;
+        document.getElementById("userName").innerHTML = currentUser;
+        const logoutElement = document.getElementById("logout");
+        logoutElement.style.cursor = "pointer";
+        if (currentUser) {
+          logoutElement.innerHTML = "Logout";
+          logoutElement.onclick = () => {
+            const confirmLogout = window.confirm(
+              "Are you sure you want to logout?"
+            );
+            console.log(confirmLogout);
+            if (confirmLogout) {
+              localStorage.removeItem("currentUser");
+              sessionStorage.removeItem("currentUser");
+            }
+          };
+        } else {
+          document.getElementById("dropdown").display = "none";
+        }
+      })
     .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
     });
--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 // Load navbar and footer dynamically
-const navbarHtml = document.querySelector('.navbar');
-const footerHtml = document.querySelector('.footer');
+const navbarHtml = document.querySelector(".navbar");
+const footerHtml = document.querySelector(".footer");
 
-fetch('./navbar/nav.html')
-    .then(response => response.text())
-    .then(html => navbarHtml.innerHTML = html)
-    .catch(error => console.error('Error loading navbar:', error));
+fetch("./navbar/nav.html")
+  .then((response) => response.text())
+  .then((html) => (navbarHtml.innerHTML = html))
+  .then(() => {
+    const currentUser =
+      localStorage.getItem("currentUser").slice(1, -1)  ||
+      sessionStorage.getItem("currentUser").slice(1, -1) ;
+    document.getElementById("userName").innerHTML = currentUser;
+    const logoutElement = document.getElementById("logout");
+    logoutElement.style.cursor = "pointer";
+    if (currentUser) {
+      logoutElement.innerHTML = "Logout";
+      logoutElement.onclick = () => {
+        const confirmLogout = window.confirm(
+          "Are you sure you want to logout?"
+        );
+        console.log(confirmLogout);
+        if (confirmLogout) {
+          localStorage.removeItem("currentUser");
+          sessionStorage.removeItem("currentUser");
+        }
+      };
+    } else {
+      document.getElementById("dropdown").display = "none";
+    }
+  })
+  .catch((error) => console.error("Error loading navbar:", error));
 
-fetch('./footer/footer.html')
-    .then(response => response.text())
-    .then(html => footerHtml.innerHTML = html)
-    .catch(error => console.error('Error loading footer:', error));
+fetch("./footer/footer.html")
+  .then((response) => response.text())
+  .then((html) => (footerHtml.innerHTML = html))
+  .catch((error) => console.error("Error loading footer:", error));
 
 // Handle category clicks
-document.querySelectorAll('.category').forEach(category => {
-    category.addEventListener('click', () => {
-        const selectedCategory = category.getAttribute('data-category');
-        window.location.href = `./products/products.html?category=${selectedCategory}`;
-    });
+document.querySelectorAll(".category").forEach((category) => {
+  category.addEventListener("click", () => {
+    const selectedCategory = category.getAttribute("data-category");
+    window.location.href = `./products/products.html?category=${selectedCategory}`;
+  });
 });
