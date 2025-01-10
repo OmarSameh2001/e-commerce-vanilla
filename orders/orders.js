22b1d190bf9ee575b9c30aa2af5639fdf63bd5e6
omar <omarsameh20017@gmail.com>
2025-01-10 21:35:38 +0200

    rename orders folder and files

--- orders/orders.js
+++ orders/orders.js
@@ -1,58 +1,55 @@
-// Navbar and footer-----------
-const navbarHtml = document.querySelector('.navbar');
+// Navbar and footer loading
+const navbarHtml = document.querySelector(".navbar");
+const footerHtml = document.querySelector(".footer");
 
-// Use fetch to load the HTML file
-fetch('../navbar/nav.html')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
+fetch("../navbar/nav.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.text();
+  })
+  .then((html) => {
+    navbarHtml.innerHTML = html;
+  })
+  .then(() => {
+    const currentUser =
+      localStorage.getItem("currentUser").slice(1, -1) ||
+      sessionStorage.getItem("currentUser").slice(1, -1);
+    document.getElementById("userName").innerHTML = currentUser;
+    const logoutElement = document.getElementById("logout");
+    logoutElement.style.cursor = "pointer";
+    if (currentUser) {
+      logoutElement.innerHTML = "Logout";
+      logoutElement.onclick = () => {
+        const confirmLogout = window.confirm(
+          "Are you sure you want to logout?"
+        );
+        if (confirmLogout) {
+          localStorage.removeItem("currentUser");
+          sessionStorage.removeItem("currentUser");
+          window.location.href = "../user/user.html";
         }
-        return response.text();
-    })
-    .then(html => {
-        navbarHtml.innerHTML = html;
-    })
-    .then(() => {
-      const currentUser =
-        localStorage.getItem("currentUser").slice(1, -1) ||
-        sessionStorage.getItem("currentUser").slice(1, -1);
-      document.getElementById("userName").innerHTML = currentUser;
-      const logoutElement = document.getElementById("logout");
-      logoutElement.style.cursor = "pointer";
-      if (currentUser) {
-        logoutElement.innerHTML = "Logout";
-        logoutElement.onclick = () => {
-          const confirmLogout = window.confirm(
-            "Are you sure you want to logout?"
-          );
-          if (confirmLogout) {
-            localStorage.removeItem("currentUser");
-            sessionStorage.removeItem("currentUser");
-            window.location.href = "../user/user.html";
-          }
-        };
-      } else {
-        document.getElementById("dropdown").display = "none";
-      }
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+      };
+    } else {
+      document.getElementById("dropdown").display = "none";
+    }
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
 
-const footerHtml = document.querySelector('.footer');
-
-// Use fetch to load the HTML file
-fetch('../footer/footer.html')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.text();
-    })
-    .then(html => {
-        footerHtml.innerHTML = html;
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+fetch("../footer/footer.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.text();
+  })
+  .then((html) => {
+    footerHtml.innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
 // Navbar and footer---------------
